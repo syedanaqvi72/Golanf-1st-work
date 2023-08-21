e19b29873d8003d5cc593ccf7e5a576c8f1cf37d
Mehak <mehakzehra5@gmail.com>
2023-08-21 12:43:56 -0700

    variables

---
+++ variable/variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//first method//
+	var a int //declaration
+	a = 55    //Initailization
+	fmt.PrintIn(a)
+	//2nd method //
+	var b int = 500
+	fmt.Print(b)
+}
