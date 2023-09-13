c6f38c6b642a2dd76a61224756fa561e1e324e55
Mehak <mehakzehra5@gmail.com>
2023-09-13 05:42:01 -0700

    work

--- variable/variable1.go
+++ variable/variable1.go
@@ -1,14 +1,14 @@
-//package main
+package main
 
-//import "fmt"
+import "fmt"
 
-//var val2 int = 100
+var val2 int = 100
 
-//func main(){
-
-//local: variables which are declared inside the function //
-//Global: variable declared outside the fuction //
-//Package-level//
-//var val = 55
-//fmt.Println(val)
-//}
+func main() {
+	//\n creates new lines in output if i write thi sin fmt.Print (val2, /n)
+	//local: variables which are declared inside the function //
+	//Global: variable declared outside the fuction //
+	//Package-level//
+	var val = 55
+	fmt.Println(val)
+}
