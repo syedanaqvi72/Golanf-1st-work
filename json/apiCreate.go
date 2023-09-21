0d54ac4234dfc88d70082debe05aa8ebab4bccd6
Mehak <mehakzehra5@gmail.com>
2023-09-21 12:13:45 -0700

    new add

--- json/apiCreate.go
+++ json/apiCreate.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	// Open the JSON file
+
 	file, err := os.Open("data.json")
 	if err != nil {
 		fmt.Println("Error opening JSON file:", err)
@@ -15,14 +15,14 @@ func main() {
 	}
 	defer file.Close()
 
-	// Decode the JSON data into the struct
+	var person Person
+
 	decoder := json.NewDecoder(file)
 	if err := decoder.Decode(&person); err != nil {
 		fmt.Println("Error decoding JSON:", err)
 		return
 	}
 
-	// Print the parsed data
 	fmt.Println("First Name:", person.FirstName)
 	fmt.Println("Last Name:", person.LastName)
 }
